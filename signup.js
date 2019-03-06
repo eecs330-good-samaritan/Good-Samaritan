e18261476a2ac875b493371dd01f379fdd9ff41f
aspungen <47461675+aspungen@users.noreply.github.com>
2019-03-06 01:23:19 -0600

    added unfinished javascript for signing up

---
+++ signup.js
@@ -0,0 +1,11 @@
+function signup()
+{
+  if (document.getElementById("t9").style.backgroundColor = "white")
+  {
+  document.getElementById("t9").style.backgroundColor = "green";
+  }
+  else if (document.getElementById("t9").style.backgroundColor = "green")
+  {
+  document.getElementById("t9").style.backgroundColor = "white";
+  }
+}

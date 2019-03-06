9c44b42d71bcd41800eee4fe825717bd4e8b5df2
Ari Jacobson <aaronjacobson2019@u.northwestern.edu>
2019-03-06 15:47:32 -0600

    added confirm shifts. Toggle does not work.

--- signup.js
+++ signup.js
@@ -1,6 +1,7 @@
 function shiftToggle(id)
 {
-  var bg = document.getElementById(id).style.backgroundColor;
+  var bg = document.getElementById(id).style.background;
+  console.log(bg)
   if (bg == "white")
   {
     bg = "green";
@@ -10,4 +11,10 @@ function shiftToggle(id)
     bg = "white";
   }
   document.getElementById(id).style.background = bg;
+}
+
+// When the user clicks on <div>, open the popup
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
 }
